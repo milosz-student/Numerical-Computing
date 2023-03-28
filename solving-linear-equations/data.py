bb4433e5029a4f49c65c1950cb642efe703ad525
P1T4G0R45 <84240013+milosz-student@users.noreply.github.com>
2023-03-28 20:36:53 +0200

    uploading files

---
+++ solving-linear-equations/data.py
@@ -0,0 +1,18 @@
+index = [1, 8, 4, 5, 7, 7]
+
+
+c = index[4]
+d = index[5]
+e = index[3]
+f = index[2]
+print(c,d,e,f)
+#N = 900+10*c+d
+#N = 900+10*c+d
+
+#print(N)
+
+a1 = 5 + e
+a2 = -1
+a3 = -1
+
+print(a1,a2,a3,f)
